afa8ed99c349b77a11f9bf0b0e653168966ac4e8
lautom36 <thomasjlau36@gmail.com>
2022-10-11 18:29:38 -0600

    fixed merge and added stuff for b

--- q1/work.py
+++ q1/work.py
@@ -49,6 +49,20 @@ def partA():
     # Traverse to edge of circle
     turn_time = 3  # seconds
     steps = int(turn_time / dt)
+def partA():
+    # circle diameter of 5m
+    # start at (0,0)
+    # constant velocity of 8m/s
+    dt = .1
+    diameter = 5
+    times = [0]
+    xs = [0]
+    ys = [0]
+    thetas = [0]
+
+    # Traverse to edge of circle
+    turn_time = 3  # seconds
+    steps = int(turn_time / dt)
     for i in range(5):
         dist_to_dia = math.sqrt(xs[-1]**2 + ys[-1]**2)
         vr = ((diameter / 2) - dist_to_dia) / turn_time
@@ -68,9 +82,35 @@ def partA():
     circumference = math.pi*diameter
 
 
+def acerman(x0, y0, theta0, alpha, vrw, L, dt=.1):
+    x1 = x0 + (-vrw) * math.sin(theta0) * dt
+    y1 = y0 + (vrw) * math.cos(theta0) * dt
+    theta1 = theta0 + (vrw/L) * math.tan(alpha) * dt
+    return x1, y1, theta1
 
-# def partB():
+def degToRad(theta):
+    return (theta * (math.pi/180))
 
+def partB():
+    xs, ys, thetas = [0], [0], [0]
+    alpha = math.radians(10)
+    L = rLength
+    vrw = avgVel
+
+    currX = 0
+    currY = 0
+    currTheta = 0
+
+    for i in range(15):
+        x, y, theta = acerman(currX, currY, currTheta, alpha, vrw, L,)
+        currX, currY, currTheta = x, y, theta
+        xs.append(x)
+        ys.append(y)
+        thetas.append(theta)
+        print("x: " + str(x) + " y: " + str(y) + " theta: " + str(theta))
+
+    print(.3246 + (-8) * math.sin(5.225) * .1)
+    plot(xs, ys)
 # def partC():
 
 partA()
