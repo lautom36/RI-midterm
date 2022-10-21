90ed5065748a92906b08fe2a2747efbb9f4f5ca8
lautom36 <thomasjlau36@gmail.com>
2022-10-20 20:05:43 -0600

    think I finished q1

--- q1/work.py
+++ q1/work.py
@@ -1,6 +1,5 @@
-
-from cProfile import label
 import math
+import time
 import matplotlib.pyplot as plt
 
 
@@ -29,11 +28,6 @@ def plotWithCircle(x, y, radius, xName = 'x - axis', yName = 'y - axis', plotNam
     ax.add_patch(circle)
     ax.add_patch(centreSpot)
 
-    # circle2 = plt.Circle((-1.25,0), radius/2, fill= False, label = "circle")
-    # centreSpot2 = plt.Circle((-1.25,0),0.05,color="black")
-    # ax.add_patch(circle2)
-    # ax.add_patch(centreSpot2)
-
     # rename axises
     plt.xlabel(xName)
     plt.ylabel(yName)
@@ -48,70 +42,59 @@ def plotWithCircle(x, y, radius, xName = 'x - axis', yName = 'y - axis', plotNam
 # global constants
 rLength = .75  # 75 cm
 rWidth = .55  # 55 cm
-
-
 avgVel = 8
 
-
-def radius(vr, w=rWidth):
-    # R(vr) = (w*4)/(vr-8)
-    return (w*4)/(vr-8)
-
-
-def theta_from_vr(vr, theta0=0, dt=.1, w=rWidth):
-    # theta. = (vr - vl) / w
-    return theta0 + (1/w)*(vr - vl_from_vr(vr))*dt
-
-
-def vl_from_vr(vr, v=avgVel):
-    # (vl + vr)/2 = v
-    # vl = 2v - vr
-    return (2*v) - vr
-
-
-def x_pos(vr, x0=0, theta0=0, dt=.1):
-    # x_(t+1) = x0 - (1/2)(vr + vl)sin(theta0)dt
-    return x0 - (1/2)*(vr + vl_from_vr(vr))*math.sin(theta0)*dt
-
-
-def y_pos(vr, y0=0, theta0=0, dt=.1):
-    # y_(t+1) = y0 - (1/2)(vr + vl)cos(theta0)dt
-    return y0 + (1/2)*(vr + vl_from_vr(vr))*math.cos(theta0)*dt
-
+def f_kinematics(vLeft, vRight, x0, y0, theta0, w, dt=0.1):
+    xNow = x0 - 1.0/2.0 * (vRight + vLeft) * math.sin(theta0) * dt
+    yNow = y0 + 1.0/2.0 * (vRight + vLeft) * math.cos(theta0) * dt
+    thetaNow = theta0 + 1/w * (vRight - vLeft) * dt
+    return(xNow, yNow, thetaNow)
 
 def partA():
     # circle diameter of 5m
     # start at (0,0)
     # constant velocity of 8m/s
     dt = .1
+    width = .55
     diameter = 5
-    times = [0]
-    xs = [0]
-    ys = [0]
-    thetas = [0]
+    times, xs, ys, thetas = [0], [0], [0], [0]
+    currX, currY, currTheta = 0, 0, 0
 
-    # Traverse to edge of circle
-    turn_time = 3  # seconds
-    steps = int(turn_time / dt)
+    # get to edge of cicle
+    rwv = 9.76
+    lwv = 6.24
     for i in range(5):
-        dist_to_dia = math.sqrt(xs[-1]**2 + ys[-1]**2)
-        vr = ((diameter / 2) - dist_to_dia) / turn_time
-        vl = vl_from_vr(vr)
-        r = radius(vr)
-        theta = theta_from_vr(vr, theta0=thetas[-1])
-        xt = x_pos(vr, x0=xs[-1], theta0=thetas[-1])
-        yt = y_pos(vr, y0=ys[-1], theta0=thetas[-1])
-        times.append(i*((diameter / 2) / turn_time))
-        xs.append(xt)
-        ys.append(yt)
-        thetas.append(theta)
-        print("Velocites: " + str(vl) + ", " + str(vr) + "\n Radius of turn: " + str(r) + "\n Theta: " + str(theta))
-    plot(xs, ys)
+        xNow, yNow, thetaNow = f_kinematics(lwv, rwv, currX, currY, currTheta, width, dt)
+        thetas.append((thetaNow - currTheta) / dt)
+        currX, currY, currTheta = xNow, yNow, thetaNow
+        xs.append(xNow)
+        ys.append(yNow)
+        times.append(times[-1] + dt)
+
+    # follow circle
+    rwv = 8.88
+    lwv = 7.12
+    for i in range(20):
+        xNow, yNow, thetaNow = f_kinematics(lwv, rwv, currX, currY, currTheta, width, dt)
+        thetas.append((thetaNow - currTheta) / dt)
+        currX, currY, currTheta = xNow, yNow, thetaNow
+        xs.append(xNow)
+        ys.append(yNow)
+        times.append(times[-1] + dt)
+
+    times.pop()
+    thetas.pop()
+    plot(xs, ys, "x position", "y position", "Position")
+    dx = [(xs[i+1]-xs[i])/dt for i in range(len(xs) - 1)]
+    plot(times, dx, "time", "trajectory, X trajectory")
+    dy = [(ys[i+1]-ys[i])/dt for i in range(len(ys) - 1)]
+    plot(times, dy, "time", "trajectory, Y trajectory")
+    plot(times, thetas, "time",  "angular velocity", "Angular Velocity as a Fuction of Time")
 
     # Stay on circle edge
     circumference = math.pi*diameter
 
-
+# start part B
 def acerman(x0, y0, theta0, alpha, vrw, L, dt=.1):
     x1 = x0 + (-vrw) * math.sin(theta0) * dt
     y1 = y0 + (vrw) * math.cos(theta0) * dt
@@ -123,7 +106,7 @@ def degToRad(theta):
 
 def partB(dtMulti = 1):
     dt = .1 / dtMulti
-    xs, ys, thetas = [0], [0], [0]
+    xs, ys, thetas, time = [0], [0], [0], [0]
     L = rLength
     vrw = avgVel
 
@@ -138,8 +121,8 @@ def partB(dtMulti = 1):
         currX, currY, currTheta = x, y, theta
         xs.append(x)
         ys.append(y)
+        time.append(time[-1] + dt)
         thetas.append(theta)
-        print("x: " + str(x) + " y: " + str(y) + " theta: " + str(math.degrees(theta)))
 
     # go around cirlce
     alpha = math.radians(16.7)
@@ -148,14 +131,20 @@ def partB(dtMulti = 1):
         currX, currY, currTheta = x, y, theta
         xs.append(x)
         ys.append(y)
+        time.append(time[-1] + dt)
         thetas.append(theta)
 
-    # print(.3246 + (-8) * math.sin(5.225) * .1)
-    # plotWithCircle(xs, ys, 2.5)
-    plot(xs, ys)
+    time.pop()
+    thetas.pop()
+    plot(xs, ys, "x position", "y position", "Position")
+    dx = [(xs[i+1]-xs[i])/dt for i in range(len(xs) - 1)]
+    plot(time, dx, "time", "trajectory, X trajectory")
+    dy = [(ys[i+1]-ys[i])/dt for i in range(len(ys) - 1)]
+    plot(time, dy, "time", "trajectory, Y trajectory")
+    plot(time, thetas, "time",  "angular velocity", "Angular Velocity as a Fuction of Time")
+# end part B
 
-dtMulti = 1
-
+# start part c
 def findXYFromLocationAngle(theta ,totalDist):
     y = math.sin(theta) * totalDist
     x = math.cos(theta) * totalDist
@@ -166,49 +155,6 @@ def findLocationAngle(circum, totalDist):
 
 def distance(x1, x2, y1, y2):
     return math.sqrt((x2-x1)**2 + (y2-y1)**2)
-
-# def partCHelper(dt):
-#     circumference = 5 * math.pi
-#     totalDist = 0
-#     xAct, yAct = [2.5], [0]
-#     totalError = 0
-#     errors = []
-#     times= []
-#     currTime = 0.0
-
-#     # start at edge of circle
-#     xs, ys, thetas = [2.5], [0], [0]
-#     L = rLength
-#     vrw = avgVel
-
-#     currX = 2.5
-#     currY = 0
-#     currTheta = 0
-
-#     # go around cirlce
-#     alpha = math.radians(16.7)
-#     for i in range(int(10 / dt)):
-#         x, y, theta = acerman(currX, currY, currTheta, alpha, vrw, L, dt)
-#         currX, currY, currTheta = x, y, theta
-#         xs.append(x)
-#         ys.append(y)
-#         thetas.append(theta)
-
-#         # find x, y, and theta actual
-#         totalDist += vrw * dt
-#         locAngle = findLocationAngle(circumference, totalDist)
-#         xActual, yActual = findXYFromLocationAngle(locAngle, totalDist)
-#         xAct.append(xActual)
-#         yAct.append(yActual)
-
-#         currTime = float(i * dt)
-
-#         if(currTime.is_integer()):
-#             totalError += distance(x, xActual, y, yActual)
-#             errors.append(totalError)
-#             times.append(i*dt)
-
-#     return times, errors
 
 def forwardEuler(dt, alpha, vrw, L, startX = 0, startY = 0, startTheta = 0):
     T = dt
@@ -230,8 +176,6 @@ def forwardEuler(dt, alpha, vrw, L, startX = 0, startY = 0, startTheta = 0):
             thetas.append(theta)
         xReturn.append(x)
         yReturn.append(y)
-
-    # plotWithCircle(xs, ys, 2.5)
 
     return xReturn, yReturn
 
@@ -260,27 +204,37 @@ def findError(xRobot, yRobot, xActual, yActual, dt):
     return errors, times
 
 def partC():
+    start = time.time()
     dt = 1
     xRobot, yRobot = forwardEuler(dt, math.radians(16.7), 8, rLength)
     xActual, yActual = findActual(dt, 8)
     print(str(len(xRobot)) + " " + str(len(xActual)))
     errors1, times1 = findError(xRobot, yRobot, xActual, yActual, dt)
-    print(len(errors1))
+    end = time.time()
+    took1 = (end - start) * 10**3
+    print("took: " + str(took1) + "ms")
 
+    start = time.time()
     dt = .1
     xRobot, yRobot = forwardEuler(dt, math.radians(16.7), 8, rLength)
     xActual, yActual = findActual(dt, 8)
     print(str(len(xRobot)) + " " + str(len(xActual)))
     errors2, times2 = findError(xRobot, yRobot, xActual, yActual, dt)
-    print(len(errors2))
+    end = time.time()
+    took2 = (end - start) * 10**3
+    print("took: " + str(took2) + "ms")
 
+    start = time.time() 
     dt = .01
     xRobot, yRobot = forwardEuler(dt, math.radians(16.7), 8, rLength)
     xActual, yActual = findActual(dt, 8)
     print(str(len(xRobot)) + " " + str(len(xActual)))
     errors3, times3 = findError(xRobot, yRobot, xActual, yActual, dt)
-    print(len(errors3))
+    end = time.time()
+    took3 = (end - start) * 10**3
+    print("took: " + str(took3) + "ms")
 
+# first plot
     plt.plot(times1,errors1, label="dt = 1")
     plt.plot(times2, errors2, label="dt = .1")
     plt.plot(times3, errors3,label="dt = .01")
@@ -296,10 +250,26 @@ def partC():
     # show graph
     plt.show()
 
+# second plot
+    data = {'dt  =1':took1, 'dt = .1':took2, 'dt = .01':took3}
+    tags = list(data.keys())
+    values = list(data.values())
+    
+    fig = plt.figure()
+    
+    # creating the bar plot
+    plt.bar(tags, values, width = 0.4)
+    
+    plt.xlabel("Courses offered")
+    plt.ylabel("No. of students enrolled")
+    plt.title("Students enrolled in different courses")
+    plt.show()
+    print()
+
+# end part c
 
 
 
-
-# partA()
-# partB(1000)
+partA()
+partB()
 partC()
